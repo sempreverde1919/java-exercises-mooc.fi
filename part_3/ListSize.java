b43de565134fd4e75fec8a5e96a6e3e226f74dff
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2023-10-17 13:22:31 +0000

    My solution

---
+++ part_3/ListSize.java
@@ -0,0 +1,25 @@
+// In the exercise template is a program that reads input from the user. 
+// Modify its working so that when the program quits reading, the program prints the number of values on the list.
+
+package part_3;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ListSize {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        
+        while(true){
+            System.out.print("Input a string: ");
+            String input = scanner.nextLine();
+            if(input.equals("")) break;
+            list.add(input);
+        }
+        scanner.close();
+        System.out.println(list.size());
+
+
+    }
+}
